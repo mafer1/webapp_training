b25085985f78376435e5c970d4c454979cc9cbea
mafer1 <ferenc312@gmail.com>
2022-04-14 09:40:22 +0200

    feat: add return_last_elements and return_most_common_elements

--- week1/cache_.py
+++ week1/cache_.py
@@ -2,49 +2,24 @@ from typing import Deque
 from collections import deque, namedtuple, Counter
 
 Element = namedtuple("Element", ["a"])
-e1= Element(1)
-e2= Element(1)
-e3= Element(1)
-e4= Element(3)
-e5= Element(3)
-e6= Element(5)
+
+list_elements = [
+    Element(1),
+    Element(1),
+    Element(1),
+    Element(3),
+    Element(3),
+    Element(5),
+]
+
+def return_last_elements(elements_list: list, num_of_element: int=5):
+    return deque(elements_list, maxlen=num_of_element)
 
 
-list_of_es = [e1, e2, e3, e4, e5]
+def return_most_common_elements(elements_list: list, most_common_num: int=1):
+    return Counter(elements_list).most_common(most_common_num)
 
-deque_of_es = deque(list_of_es, maxlen=4)
-# most common
-es = Counter(deque_of_es)
-es.most_common()
-
-# last n elements
-
-class Cache:
-    def __init__(self) -> None:
-        self.elements= []
-        self.max_len = 5
-  
-    def append_element(self, element):
-        self.elements.append(element)
-
-    def return_elements(self):
-        return deque(self.elements, maxlen=self.max_len)
-
-    def return_most_common(self):
-        return Counter(self.elements)
-        
-cla = Cache()
-cla.append_element("xD")
-cla.append_element("xD")
-cla.append_element("xD")
-cla.append_element("xD")
-cla.append_element("xD")
-cla.append_element("xD")
-
-cla.elements
-
-c = cla.return_elements()
-print(c)
 
 if __name__ == "__main__":
-	print(cla.elements)
+    print(f"Most common elements: {return_most_common_elements(list_elements, 2)}")
+    print(f"Most recent elements: {return_last_elements(list_elements, 2)}")
