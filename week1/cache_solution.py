1cd17bc361f14ad437fc24e50055d89300679b74
mafer1 <ferenc312@gmail.com>
2022-04-14 15:34:28 +0200

    feat: add Point class

---
+++ week1/cache_solution.py
@@ -0,0 +1,12 @@
+from typing import NamedTuple, Union
+
+
+class Point(NamedTuple):
+    """The Point representation in three-dimensional space"""
+
+    x: Union[int, float] = 0.0
+    y: Union[int, float] = 0.0
+    z: Union[int, float] = 0.0
+
+    def __repr__(self) -> str:
+        return f"Point coordinates x:{self.x}, y:{self.y}, z:{self.z}"
