77d20455fe7f340d20fbf09f885775882a42a378
mafer1 <ferenc312@gmail.com>
2022-04-15 18:10:44 +0200

    refactor: linting black

--- week1/cache_solution.py
+++ week1/cache_solution.py
@@ -22,7 +22,7 @@ class Storage:
 
     def put(self, *args):
         _point: Point = args[0] if isinstance(args[0], Point) else Point(args[0], args[1], args[2])
-        _id = random.randint(0,10000)
+        _id = random.randint(0, 10000)
         self.storage.append(_point)
         self.cache.lru_cache.put(_id, _point)
         self.cache.lfu_cache.put(_id, _point)
@@ -66,8 +66,8 @@ class Storage:
                 self.key = key
                 self.value = value
                 self.freq_node = freq_node
-                self.pre = pre # previous CacheNode
-                self.nxt = nxt # next CacheNode
+                self.pre = pre  # previous CacheNode
+                self.nxt = nxt  # next CacheNode
 
             def free_myself(self):
                 if self.freq_node.cache_head == self.freq_node.cache_tail:
@@ -89,10 +89,10 @@ class Storage:
         class FreqNode(object):
             def __init__(self, freq, pre, nxt):
                 self.freq = freq
-                self.pre = pre # previous FreqNode
-                self.nxt = nxt # next FreqNode
-                self.cache_head = None # CacheNode head under this FreqNode
-                self.cache_tail = None # CacheNode tail under this FreqNode
+                self.pre = pre  # previous FreqNode
+                self.nxt = nxt  # next FreqNode
+                self.cache_head = None  # CacheNode head under this FreqNode
+                self.cache_tail = None  # CacheNode tail under this FreqNode
 
             def count_caches(self):
                 if self.cache_head is None and self.cache_tail is None:
@@ -100,7 +100,7 @@ class Storage:
                 elif self.cache_head == self.cache_tail:
                     return 1
                 else:
-                    return '2+'
+                    return "2+"
 
             def remove(self):
                 if self.pre is not None:
@@ -144,24 +144,23 @@ class Storage:
 
                 if self.nxt is not None:
                     self.nxt.pre = freq_node
-                
+
                 self.nxt = freq_node
 
             def insert_before_me(self, freq_node):
                 if self.pre is not None:
                     self.pre.nxt = freq_node
-                
+
                 freq_node.pre = self.pre
                 freq_node.nxt = self
                 self.pre = freq_node
-                
-        class LFUCache(object):
 
+        class LFUCache(object):
             def __init__(self, capacity):
                 self.cache = OrderedDict()
                 self.capacity = capacity
                 self.freq_link_head = None
-            
+
             def get(self, key):
                 if key in self.cache:
                     cache_node = self.cache[key]
@@ -177,7 +176,7 @@ class Storage:
             def put(self, key, value):
                 if self.capacity <= 0:
                     return -1
-                
+
                 if key not in self.cache:
                     if len(self.cache) >= self.capacity:
                         self.dump_cache()
@@ -197,13 +196,12 @@ class Storage:
                 else:
                     target_freq_node = freq_node.nxt
                     target_empty = False
-                
+
                 cache_node.free_myself()
                 target_freq_node.append_cache_to_tail(cache_node)
 
                 if target_empty:
                     freq_node.insert_after_me(target_freq_node)
-
 
                 if freq_node.count_caches() == 0:
                     if self.freq_link_head == freq_node:
@@ -223,14 +221,14 @@ class Storage:
             def create_cache(self, key, value):
                 cache_node = CacheNode(key, value, None, None, None)
                 self.cache[key] = cache_node.value
-                
+
                 if self.freq_link_head is None or self.freq_link_head.freq != 0:
                     new_freq_node = FreqNode(0, None, None)
                     new_freq_node.append_cache_to_tail(cache_node)
 
                     if self.freq_link_head is not None:
                         self.freq_link_head.insert_before_me(new_freq_node)
-                    
+
                     self.freq_link_head = new_freq_node
                 else:
                     self.freq_link_head.append_cache_to_tail(cache_node)
