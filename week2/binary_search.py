7925f832df44abefaa57e6ef15430ae235ebcaac
mafer1 <ferenc312@gmail.com>
2022-04-26 10:34:44 +0200

    feat: add BinarySearch

---
+++ week2/binary_search.py
@@ -0,0 +1,10 @@
+
+
+class BinarySearch:
+    """Binary search"""
+
+    def __init__(self, value, start=0, end=100):
+        self.start = start
+        self.end = end
+        self.value = value
+        self.steps_count = 0
